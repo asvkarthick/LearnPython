bde274e0d7dfc12dd1c62d2958336a3455cf8260
Karthick Kumaran <asvkarthick@gmail.com>
2019-08-05 17:13:29 -0700

    Added python program to get current system time
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 06-time/time-02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+import time
+
+print('Sleeping for 2 seconds')
+print("Current time is " + time.ctime())
+time.sleep(2)
+print('Done')
+print("Current time is " + time.ctime())
