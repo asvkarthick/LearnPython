f5754a50d9873bd05fd7dd08702712eaa463b4db
Karthick Kumaran <asvkarthick@gmail.com>
2019-08-05 22:54:30 -0700

    Added python program to use inbuilt functions
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 02-function/function-03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+list_num = [9, 8, 7, 6]
+sorted_list = sorted(list_num)
+print('list_num = ', list_num)
+print('sorted_list = ', sorted_list)
