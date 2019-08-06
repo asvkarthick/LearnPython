b7f665c5dedd95ba030bd31ffd1176cc5f96706b
Karthick Kumaran <asvkarthick@gmail.com>
2019-08-06 09:30:15 -0700

    Another simple numpy python program to demonstrate zeros ones identical matrix
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 09-numpy/numpy-02.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+import numpy as np
+
+a = np.zeros((2, 2))
+print(a)
+
+b = np.ones((2, 2))
+print(b)
+
+c = np.eye(2)
+print(c)
+
+d = np.full((2, 2), 7)
+print(d)
