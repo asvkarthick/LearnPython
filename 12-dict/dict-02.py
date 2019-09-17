5adcc282776b0403afdedac2de0fb99f0c009b86
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-16 23:26:01 -0700

    Updated dict-02 program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

--- 12-dict/dict-02.py
+++ 12-dict/dict-02.py
@@ -10,3 +10,7 @@ print('Printing dictionary with loop')
 for i in d:
     print(i, end = ' ', flush = True)
 print()
+
+print('Printing both key and values of dictionary with loop')
+for k,v in d.items() :
+    print('{} : {}'.format(k,v))
