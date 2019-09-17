e46a381963bf66d9d527768e21350940af5579db
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-16 23:08:08 -0700

    Created simple dictionary program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 12-dict/dict-01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+print('Creating a dictionary')
+d = { 1 : 'One', 2 : 'Two', 3 : 'Three', 4 : 'Four', 5 : 'Five' }
+print(d)
+print(type(d))
