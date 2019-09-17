54176da66f05168abf203f99111233c1beb50754
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-16 23:58:11 -0700

    Simple set program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 13-set/set-01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+set_a = set("ABCDE")
+set_b = set("CDEFG")
+
+print('Printing set a')
+print(set_a)
+print('Printing set b')
+print(set_b)
+print(type(set_a))
+print(type(set_b))
