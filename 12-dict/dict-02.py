b16b40b4060fc63caed4e23c0f9fdc359a839882
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-16 23:24:07 -0700

    Added another dict program to print dict contents with loop
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 12-dict/dict-02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+print('Creating a dictionary')
+d = { 1 : 'One', 2 : 'Two', 3 : 'Three', 4 : 'Four', 5 : 'Five' }
+print(d)
+print(type(d))
+
+print('Printing dictionary with loop')
+for i in d:
+    print(i, end = ' ', flush = True)
+print()
