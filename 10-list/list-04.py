6e0c3afb46babeccf91f234cff2b0c74ad6efc9c
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-16 19:23:24 -0700

    Added list-04 to demo update of list
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 10-list/list-04.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+print('Create a list')
+l = [1, 2, 3, 4, 5]
+print(l)
+
+print('Append 6 to the list')
+l.append(6)
+print(l)
+
+print('Insert 100 to the beginning of the list')
+l.insert(0, 100)
+print(l)
+
+print('Update the second element to 200')
+l[1] = 200
+print(l)
+
+print('Remove the last element from the list')
+l.pop()
+print(l)
+
+print('Removing element at index 1')
+l.remove(1)
+print(l)
+
+print('Remove element at index 2')
+del l[2]
+print(l)
+
+print('Remove elements from index 1 to 4')
+del l[1:4]
+print(l)
