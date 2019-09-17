668581c35c9945e9dd28d0b76e92402e894e92c8
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-16 23:59:26 -0700

    Another simple set program with different set operations
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 13-set/set-02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+set_a = set("ABCDE")
+set_b = set("CDEFG")
+
+print('Printing set a')
+print(set_a)
+print('Printing set b')
+print(set_b)
+print(type(set_a))
+print(type(set_b))
+
+print(set_a - set_b)
+print(set_a | set_b)
+print(set_a ^ set_b)
+print(set_a & set_b)
