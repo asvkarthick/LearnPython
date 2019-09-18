d4406b22451303403f887a8d5f1df68e7d2fd42e
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-17 17:37:08 -0700

    Simple string manipulation program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 15-string/string-01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# Author : Karthick Kumaran
+
+s = 'Karthick'
+print('Original string: {}'.format(s))
+print('UPPER : {}'.format(s.upper()))
+print('lower : {}'.format(s.lower()))
+print('Capitalize : {}'.format(s.capitalize()))
