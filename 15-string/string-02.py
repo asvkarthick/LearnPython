2f19109e7bfbc2e975c305ea5813e6d0d0dfd1e1
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-17 17:37:22 -0700

    Another simple string manipulation program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ 15-string/string-02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# Author : Karthick Kumaran
+
+s1 = 'Karthick'
+s2 = 'Kumaran'
+s3 = s1 + s2
+s4 = s1 + ' ' + s2
+print('s1 = ' + s1)
+print('s2 = ' + s2)
+print('s3 = ' + s3)
+print('s4 = ' + s4)
