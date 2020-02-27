cf288836f5e133f0556015c550e3e669bdfb2be4
Karthick Kumaran <asvkarthick@gmail.com>
2020-02-26 23:49:28 -0800

    Add another stock analysis program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ StockAnalysis/stock-06.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+import pandas as pd
+
+tickers = ["AMZN", "AAPL", "MSFT", "GOOG", "FB"]
+
+data = yf.download(tickers, start="2020-02-20", end="2020-02-26", group_by="ticker")
+
+close = []
+diff = []
+for c in tickers:
+    close.append(data[c]['Close'])
+    # d = data[c]['Close'][0]
+    d = 0
+    i = 1
+    while i < len(data):
+        d = d + data[c]['Close'][i] - data[c]['Close'][i - 1]
+        i = i + 1
+    diff.append(d)
+
+df = pd.concat(close, axis = 1, keys = tickers)
+print(df)
+
+print()
+print("After Corona-Virus stocks difference")
+df_diff = pd.DataFrame(diff, index = tickers)
+print(df_diff)
