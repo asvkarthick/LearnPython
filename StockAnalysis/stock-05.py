edd480647345ad2ca607b0efbd602e481370a682
Karthick Kumaran <asvkarthick@gmail.com>
2020-02-26 22:49:23 -0800

    Simple Python program with Pandas to display AMZN and AAPL stocks close values next to each other
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

--- StockAnalysis/stock-05.py
+++ StockAnalysis/stock-05.py
@@ -1,4 +1,10 @@
 import yfinance as yf
+import pandas as pd
+
 data = yf.download("AMZN AAPL", start="2020-01-01", end="2020-02-20", group_by="ticker")
-print(data)
-print(data['AMZN']['Close'])
+close_amzn = data['AMZN']['Close']
+close_aapl = data['AAPL']['Close']
+
+close = [close_amzn, close_aapl]
+df = pd.concat(close, axis = 1, keys = ['AMZN', 'AAPL'])
+print(df)
