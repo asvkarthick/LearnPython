ee593f4456fd51fdaaf0a383aebe88fb3a1abeff
Karthick Kumaran <asvkarthick@gmail.com>
2020-04-05 20:28:40 -0700

    Hello world GUI program
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ GUI/tkinter/tkinter-01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author: Karthick Kumaran <asvkarthick@gmail.com>
+
+import tkinter as tk
+
+# Create instance
+win = tk.Tk()
+
+# Set the title for the window
+win.title("Python GUI Hello World")
+
+# Start the GUI
+win.mainloop()
